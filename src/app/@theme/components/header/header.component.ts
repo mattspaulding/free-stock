9c963418ea692eaff679eb976b18149e24a713f0
Matt Spaulding <matthew.spaulding@iprocedures.com>
2017-11-28 01:44:46 -0500

    Proper search redirect from header.

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -3,6 +3,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {NbMenuService, NbSidebarService} from '@nebular/theme';
 import {UserService} from '../../../@core/data/users.service';
 import {AnalyticsService} from '../../../@core/utils/analytics.service';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'ngx-header',
@@ -23,7 +24,8 @@ export class HeaderComponent implements OnInit {
   constructor(private sidebarService: NbSidebarService,
               private menuService: NbMenuService,
               private userService: UserService,
-              private analyticsService: AnalyticsService) {
+              private analyticsService: AnalyticsService,
+              private router:Router) {
   }
 
   ngOnInit() {
@@ -103,8 +105,9 @@ export class HeaderComponent implements OnInit {
     this.menuService.navigateHome();
   }
 
-  startSearch() {
-    this.analyticsService.trackEvent('startSearch');
+  search() {
+    this.router.navigate(["stock/search"]);
+
   }
 
   getUser() {
