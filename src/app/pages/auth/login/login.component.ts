00d26b57a3283b3e4a1ab41166d53086e4403480
Matt Spaulding <matthew.spaulding@iprocedures.com>
2018-01-26 23:01:36 -0500

    Debugging login.

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../../@core/data/users.service";
+import {isLoop} from "tslint";
 
 @Component({
   selector: 'ngx-login',
@@ -7,11 +8,31 @@ import {UserService} from "../../../@core/data/users.service";
   templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit{
-
+isLoggedIn:string;
   constructor(private userService:UserService){
-
+this.isLoggedIn="dunno"
   }
   ngOnInit(){
-    this.userService.login();
+    this.userService.login()
+      .then((res: any) => {
+        console.log('Logged in', res);
+        debugger;
+        this.isLoggedIn="logged in";
+        // if (res.authResponse.grantedScopes.includes('email')) {
+        //   localStorage.accessToken = res.authResponse.accessToken;
+        //   this.router.navigate([""]);
+        //   location.reload()
+        //   return res;
+        // } else {
+        //   this.login();
+        // }
+      })
+      .catch((err) => {
+        debugger;
+        this.isLoggedIn="problem with login"
+        alert('problem logging in:'+err)
+        // return false;
+      });
+
   }
 }
