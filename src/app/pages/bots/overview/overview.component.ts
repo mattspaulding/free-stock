a13adb59cd130705d70a3e7d2d9d22fedac35746
Matt Spaulding <matthew.spaulding@iprocedures.com>
2018-06-23 23:09:05 -0400

    Testing fb bot.

--- src/app/pages/bots/overview/overview.component.ts
+++ src/app/pages/bots/overview/overview.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var window: any;
 
 @Component({
   selector: 'ngx-overview',
   styleUrls: ['./overview.component.scss'],
   templateUrl: './overview.component.html',
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
 
+  constructor() {
+  }
+
+  ngOnInit() {
+    window.FB.XFBML.parse();
+    }
 }
